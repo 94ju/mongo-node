cfcda49905d7e88122a9d2e6d41d8dfe40094b7f
94ju <janithherath94@gmail.com>
2019-06-28 23:56:30 +0530

    schema created

--- index.js
+++ index.js
@@ -7,4 +7,33 @@ mongoose.connect('mongodb://localhost/playgroungcheck').
         }
     ).catch(
         error=>console.log('connection failed',error)
-    )
+    );
+
+const courseSchema= new mongoose.Schema(
+    {
+        name:String,
+        author:String,
+        tags:[String],
+        date:{
+            type:Date,
+            default:Date.now
+        },
+        isPublished:Boolean
+    }
+)
+
+const Course = mongoose.model('Course',courseSchema);
+const course = new Course(
+    {
+        name:'NodeJs',
+        author:'Jnanith',
+        tags:['node','backend'],
+        isPublished:true
+    }
+);
+
+course.save().then(
+    (result)=>console.log(result)
+).catch(
+    error=>console.log(error)
+)
